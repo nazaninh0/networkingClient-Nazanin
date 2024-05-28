ffe3f7c4e53f4980aa9803dac144b8075ee4efe3
nazaninh0 <nazanin.heydari@learnet.se>
2024-05-28 18:58:06 +0200

    add some code

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+/* CLIENT */
+import java.io.*;
+import java.net.*;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Socket client = null;
 
-        // Press Skift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Skift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Default port number we are going to use
+        int portnumber = 1234;
+        if (args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
         }
+
+
     }
 }
