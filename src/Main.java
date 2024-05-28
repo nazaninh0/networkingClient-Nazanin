662bb03a869b2b2b0cea74e53b3df20226f6fa6a
nazaninh0 <nazanin.heydari@learnet.se>
2024-05-28 19:04:47 +0200

    add some code

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,23 @@ public class Main {
         if (args.length >= 1){
             portnumber = Integer.parseInt(args[0]);
         }
+        for (int i=0 ;i<10; i++){
+            try{
+                String msg="";
+                client = new Socket(InetAddress.getLocalHost(), portnumber);
+                System.out.println("Client socket is created " + client);
+                OutputStream clientOut = client.getOutputStream();
+                PrintWriter pw = new PrintWriter(clientOut,true);
+                InputStream clientIn= client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("enter your name ,Type bye to exit");
+                msg = stdIn.readLine().trim();
+                pw.println(msg);
+                System.out.println("Message returned from the server = " + br.readLine());
+
+            }
+        }
 
 
     }
